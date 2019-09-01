c21b65b41fff5a92a3243a4361c14d6b9df4b7b5
wangzilong <1321787581@qq.com>
2019-09-02 07:50:08 +0800

    header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { GlobalStyle } from './style';
+import Header from './common/header'
+
+function App() {
+  return (
+    <div>
+      <Header />
+     <GlobalStyle />
+     helloworld
+    </div>
+  );
+}
+
+export default App;
---
+++ src/common/header/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  HeaderWrapper,
+  Logo,
+  Nav,
+  NavItem,
+  NavSearch,
+  Addition,
+  Button
+ } from './style';
+function Header() {
+  return (
+    <HeaderWrapper>
+      <Logo />
+      <Nav>
+        <NavItem className="left active">首页</NavItem>
+        <NavItem className="left">下载app</NavItem>
+        <NavItem className="right">登陆</NavItem>
+        <NavItem className="right">Aa</NavItem>
+        <NavSearch></NavSearch>
+      </Nav>
+      <Addition>
+        <Button className="writting">写文章</Button>
+        <Button className="reg">注册</Button>
+      </Addition>
+    </HeaderWrapper>
+  )
+}
+export default Header;
